abb27530f781b89463ec54f405756cfa7a9a9c46
Alaa Adel <alaaadel@192.168.220.128>
2020-04-25 19:23:23 +0200

    Initial Commit

---
+++ YTSMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YTSMovies
+//
+//  Created by Alaa Adel on 4/25/20.
+//  Copyright © 2020 Alaa Adel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
